9217e2968034bcda1fe7a94529759c241d3649ee
8xhamhoc <tomatoquang@gmail.com>
2022-04-28 22:10:41 +0700

    Working with autoconfigure in spring boot.

--- src/main/java/org/quangphan/spring/transaction/SpringTransactionApplication.java
+++ src/main/java/org/quangphan/spring/transaction/SpringTransactionApplication.java
@@ -1,13 +1,19 @@
 package org.quangphan.spring.transaction;
 
+import org.quangphan.spring.transaction.config.DatabaseConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@EnableConfigurationProperties({DatabaseConfigProperties.class})
+@ConfigurationPropertiesScan("org.quangphan.spring.transaction.config")
 public class SpringTransactionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTransactionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionApplication.class, args);
+    }
 
 }
---
+++ src/main/java/org/quangphan/spring/transaction/config/TxConfig.java
@@ -0,0 +1,9 @@
+package org.quangphan.spring.transaction.config;
+
+import javax.sql.DataSource;
+
+public interface TxConfig {
+
+    DataSource dataSource();
+
+}
